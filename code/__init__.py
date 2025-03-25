3460ed4983abd3517da44acf50f94eb395705d61
cristianobraga <134560514+Cr1st1ano88@users.noreply.github.com>
2025-03-25 12:34:41 -0300

    correção game

---
+++ code/__init__.py
@@ -0,0 +1,6 @@
+class Game:
+    pass
+
+
+class Menu:
+    pass
--- code/game.py
+++ code/game.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from code.menu import Menu  # Importação corrigida
 
 class Game:
     def __init__(self):
-        self.window = None
-
-    def run(self, ):
         pass
+
+        pygame.init()
+        self.window = pygame.display.set_mode((600, 480))  # Corrigido: agora usa self.window
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)  # Correção do nome da classe
+            menu.run()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Fecha a janela
+                    quit()  # Encerra o pygame
