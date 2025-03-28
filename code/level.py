a524d5d1cc11f737fcf74b6297e7a3bfcf57037d
cristianobraga <134560514+Cr1st1ano88@users.noreply.github.com>
2025-03-28 15:54:51 -0300

    Creating mediator pattern. enemies destruction

--- code/level.py
+++ code/level.py
@@ -7,6 +7,8 @@ import pygame.display
 from pygame import Surface, Rect
 from pygame.font import Font
 
+from code.entetymediator import EntityMediator
+
 sys.path.append(r'D:\Documents\lilian\MountainShooter\code')
 
 from code.entity import Entity
@@ -50,6 +52,9 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            # Collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
         pass
 
     def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
